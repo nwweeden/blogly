be58d38ee58bd49c0a191a17d838c63df9163e20
Nick Weeden <nicholas_weeden@alumni.brown.edu>
2020-07-30 12:54:21 -0700

    included model for posts

--- models.py
+++ models.py
@@ -21,4 +21,20 @@ class User(db.Model):
     first_name = db.Column(db.String(50), nullable=False, default='')
     last_name = db.Column(db.String(50), nullable=False)
     image_url = db.Column(db.String, nullable=False, default='')  #should we include a default image???
-  
+
+    posts = db.relationship('Post')
+
+class Post(db.Model):
+    '''Post.'''
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.integer, primary_key=True)
+    title = db.Column(db.string(50), nullable=False)
+    content = db.Column(db.string(1000), nullable=False, default='')
+    created_at = db.Column(db.'timestammp with time zone',nullable=false, default=Current_Timestamp) #is this how you do timestamp?
+    user_id = db.column(
+        db.string, #what's the difference between txt and string?
+        db.ForeignKey(users.id))
+    
+    user = db.relationship('User')
